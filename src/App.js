b3390fb1fc1f8e9d6b01d9a4b6e67db2d8fdf9b5
DongHa2021 <96828648+DongHa2021@users.noreply.github.com>
2022-05-26 13:03:28 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import "./styles.css";
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editidfdfng to see some magic happen!</h2>
+      <h1>Term Project</h1>
+      <h2>20180134 김동하</h2>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+      ;
     </div>
   );
 }
